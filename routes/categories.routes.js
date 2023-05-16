b7abf5450fc93bc879f5aa88e2d0ae8dc636aaab
Santiago Massa <sjm_95@hotmail.com>
2023-05-16 13:12:51 +0200

    categories routes

--- routes/categories.routes.js
+++ routes/categories.routes.js
@@ -1,16 +1,78 @@
 // require and use express:
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+// Require the Character model:
+const Character = require("../models/Character.model");
+
 // require and destructure the middleware:
-const {isLoggedIn} = require("../middlewares/auth.middlewares")
+const { isLoggedIn } = require("../middlewares/auth.middlewares");
 
 // GET "/categories/" => Render a list of all categories:
 router.get("/", isLoggedIn, (req, res, next) => {
-    res.render("categories/list")
-})
+  res.render("categories/list");
+});
 
 // Use dynamic routes to render different list of categories's list.
+// GET "/categories/species" => Render a list of all species.
+router.get("/species", isLoggedIn, (req, res, next) => {
+  // distinct works doing ...
+  Character.distinct("species")
+    .then((speciesList) => {
+      //console.log(speciesList)
+      res.render("categories/species", {
+        speciesList: speciesList,
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+// GET "/categories/homeworlds" => Render a list of all homeworlds.
+router.get("/homeworlds", isLoggedIn, (req, res, next) => {
+  Character.distinct("homeworld")
+    .then((homeworldsList) => {
+      console.log(homeworldsList);
+      res.render("categories/homeworlds", {
+        homeworldsList: homeworldsList,
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+// GET "/categories/:species/list" => render list of characters by specific specie
+router.get("/:species/list", (req, res, next) => {
+  Character.find({ species: req.params.species })
+    .then((allCharBySpecies) => {
+      console.log(allCharBySpecies);
+      res.render("categories/species-list", {
+        allCharBySpecies: allCharBySpecies,
+        oneSpecies: req.params.species,
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
+  //console.log(req.params)
+});
+
+// GET "/categories/:homeworlds/list" => render list of characters by specific homeworld
+router.get("/:homeworld/homeworlds-list", (req, res, next) => {
+  Character.find({ homeworld: req.params.homeworld })
+    .then((allCharByHomeworld) => {
+      console.log(allCharByHomeworld);
+      res.render("categories/homeworlds-list", {
+        allCharByHomeworld: allCharByHomeworld,
+        oneHomeworld: req.params.homeworld,
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 // export the routes:
 module.exports = router;
